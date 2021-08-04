3f32192cc3b22f3aeb3791daae355583e1060490
Kait <kweishaar@hawk.iit.edu>
2021-08-04 11:09:44 -0500

    error isn't working

--- server.js
+++ server.js
@@ -40,6 +40,12 @@ function filterbyQuery(query, animalsArray) {
     return filteredResults;
 };
 
+
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -47,6 +53,15 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req,res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
 app.listen(PORT, () => {
     console.log(`API serve now on port ${PORT}!`);
 });
